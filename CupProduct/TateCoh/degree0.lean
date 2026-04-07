fafa4f27fa4adc008eb1ea86afb03fdee8c472aa
Whysoserioushah <yx3021@ic.ac.uk>
2026-04-07 17:09:45 +0100

    fix degree0 for tate

--- CupProduct/TateCoh/degree0.lean
+++ CupProduct/TateCoh/degree0.lean
@@ -4,7 +4,6 @@ import CupProduct.groupCoh.UpIso
 import CupProduct.groupCoh.DownIso
 import CupProduct.groupCoh.degree0
 
-#exit
 open CategoryTheory groupCohomology MonoidalCategory Rep.dimensionShift
 
 universe u
@@ -22,14 +21,12 @@ abbrev cup0Aux' (A B : Rep R G) (a : A.ρ.invariants) :
     (A ⊗ B).ρ.invariants ⧸ (LinearMap.range (A ⊗ B).ρ.norm).submoduleOf (A ⊗ B).ρ.invariants :=
   Submodule.mapQ (R₂ := R) (τ₁₂ := .id R) _ _ (_root_.cup0Aux' A B a) <| fun x hx ↦ by
     simp only [Submodule.submoduleOf, Submodule.mem_comap, Submodule.subtype_apply,
-      LinearMap.mem_range, Action.tensorObj_V, Rep.tensor_ρ, _root_.cup0Aux',
-      Equivalence.symm_inverse, Action.functorCategoryEquivalence_functor,
-      Action.FunctorCategoryEquivalence.functor_obj_obj, LinearMap.coe_mk, AddHom.coe_mk] at hx ⊢
+      LinearMap.mem_range, Rep.tensor_V, Rep.tensor_ρ, _root_.cup0Aux', LinearMap.coe_mk,
+      AddHom.coe_mk] at hx ⊢
     obtain ⟨b, hb⟩ := hx
     use a ⊗ₜ b
-    simp only [ModuleCat.MonoidalCategory.tensorObj_carrier, Representation.norm,
-      Representation.tprod_apply, LinearMap.coe_sum, Finset.sum_apply, TensorProduct.map_tmul,
-      ← hb, TensorProduct.tmul_sum]
+    simp only [Representation.norm, Representation.tprod_apply, LinearMap.coe_sum, Finset.sum_apply,
+      TensorProduct.map_tmul, ← hb, TensorProduct.tmul_sum]
     exact Finset.sum_congr rfl fun g _ ↦ by rw [a.2 g]
 
 @[simps]
@@ -39,50 +36,46 @@ def cup0AuxAux (A B : Rep R G) : A.ρ.invariants →ₗ[R] B.ρ.invariants ⧸
   toFun a := cup0Aux' A B a
   map_add' a1 a2 := by
     ext b
-    simp only [Action.tensorObj_V, Rep.tensor_ρ, cup0Aux', LinearMap.coe_comp, Function.comp_apply,
+    simp only [Rep.tensor_ρ, cup0Aux', LinearMap.coe_comp, Function.comp_apply,
       Submodule.mkQ_apply, Submodule.mapQ_apply, LinearMap.add_apply]
     change Submodule.Quotient.mk (cup0Aux A B (a1 + a2) b) = _
     rw [map_add, LinearMap.add_apply, Submodule.Quotient.mk_add]
     simp [cup0Aux]
   map_smul' r a := by
     ext b
-    simp only [Action.tensorObj_V, Rep.tensor_ρ, cup0Aux', LinearMap.coe_comp, Function.comp_apply,
+    simp only [Rep.tensor_ρ, cup0Aux', LinearMap.coe_comp, Function.comp_apply,
       Submodule.mkQ_apply, Submodule.mapQ_apply, RingHom.id_apply, LinearMap.smul_apply]
     change Submodule.Quotient.mk (cup0Aux _ _ _ _) = _
     rw [map_smul, LinearMap.smul_apply, Submodule.Quotient.mk_smul]
     simp [cup0Aux]
 
+set_option backward.isDefEq.respectTransparency false in
 abbrev cup0Aux (A B : Rep R G) : A.ρ.invariants ⧸
     (LinearMap.range A.ρ.norm).submoduleOf A.ρ.invariants →ₗ[R] B.ρ.invariants ⧸
     (LinearMap.range B.ρ.norm).submoduleOf B.ρ.invariants →ₗ[R]
     (A ⊗ B).ρ.invariants ⧸ (LinearMap.range (A ⊗ B).ρ.norm).submoduleOf (A ⊗ B).ρ.invariants :=
   Submodule.liftQ _ (cup0AuxAux A B) <| fun a ha ↦ by
     simp only [Submodule.submoduleOf, Submodule.mem_comap, Submodule.subtype_apply,
-      LinearMap.mem_range, Action.tensorObj_V, Rep.tensor_ρ, cup0AuxAux, cup0Aux',
-      LinearMap.mem_ker, LinearMap.coe_mk, AddHom.coe_mk] at ha ⊢
+      LinearMap.mem_range, Rep.tensor_V, Rep.tensor_ρ, cup0AuxAux, cup0Aux',
+      LinearMap.mem_ker] at ha ⊢
     ext b
-    simp only [LinearMap.coe_comp, Function.comp_apply, Submodule.mkQ_apply, Submodule.mapQ_apply,
-      LinearMap.zero_comp, LinearMap.zero_apply, Submodule.Quotient.mk_eq_zero, Submodule.mem_comap,
+    simp only [Rep.tensor_V, Rep.tensor_ρ, LinearMap.coe_mk, AddHom.coe_mk, LinearMap.coe_comp,
+      Function.comp_apply, Submodule.mkQ_apply, Submodule.mapQ_apply, LinearMap.zero_comp,
+      LinearMap.zero_apply, Submodule.Quotient.mk_eq_zero, Submodule.mem_comap,
       Submodule.subtype_apply, LinearMap.mem_range]
     obtain ⟨a', ha'⟩ := ha
     use a' ⊗ₜ b.1
-    simp only [ModuleCat.MonoidalCategory.tensorObj_carrier, Representation.norm,
-      Representation.tprod_apply, LinearMap.coe_sum, Finset.sum_apply, TensorProduct.map_tmul,
-      _root_.cup0Aux', Action.tensorObj_V, Rep.tensor_ρ, Equivalence.symm_inverse,
-      Action.functorCategoryEquivalence_functor, Action.FunctorCategoryEquivalence.functor_obj_obj,
-      ← ha', TensorProduct.sum_tmul, LinearMap.coe_mk, AddHom.coe_mk]
+    simp only [Representation.norm, Representation.tprod_apply, LinearMap.coe_sum, Finset.sum_apply,
+      TensorProduct.map_tmul, _root_.cup0Aux', Rep.tensor_V, Rep.tensor_ρ, ← ha',
+      TensorProduct.sum_tmul, LinearMap.coe_mk, AddHom.coe_mk]
     exact Finset.sum_congr rfl fun g _ ↦ by rw [b.2 g]
 
 def cup0 (A B : Rep R G) : (tateCohomology 0).obj A →ₗ[R] (tateCohomology 0).obj B →ₗ[R]
     (tateCohomology 0).obj (A ⊗ B) where
   toFun a := (TateCohomology.zeroIso (A ⊗ B)).inv.hom ∘ₗ
     cup0Aux A B (TateCohomology.zeroIso A|>.hom.hom a) ∘ₗ (TateCohomology.zeroIso B).hom.hom
-  map_add' a1 a2 := by
-    simp only [Action.tensorObj_V, ModuleCat.MonoidalCategory.tensorObj_carrier, Rep.tensor_ρ,
-      map_add]
-    rw [LinearMap.add_comp, LinearMap.comp_add]
-  map_smul' r a := by
-    simp [ModuleCat.MonoidalCategory.tensorObj_carrier, LinearMap.smul_comp, LinearMap.comp_smul]
+  map_add' a1 a2 := by simp [LinearMap.add_comp, LinearMap.comp_add]
+  map_smul' r a := by simp [LinearMap.smul_comp, LinearMap.comp_smul]
 
 abbrev cup0' (A B : Rep R G) : (tateCohomology 0).obj A ⊗ (tateCohomology 0).obj B ⟶
     (tateCohomology 0).obj (A ⊗ B) :=
@@ -90,17 +83,15 @@ abbrev cup0' (A B : Rep R G) : (tateCohomology 0).obj A ⊗ (tateCohomology 0).o
 
 variable (R G) in
 @[simps]
-def Rep.invariantsQuotFunctor : Rep R G ⥤ ModuleCat R where
+def Rep.invariantsQuotFunctor : Rep.{u} R G ⥤ ModuleCat R where
   obj A := ModuleCat.of R <| A.ρ.invariants ⧸ (LinearMap.range A.ρ.norm).submoduleOf A.ρ.invariants
   map {A B} f := ModuleCat.ofHom <| Submodule.mapQ _ _
       ((Rep.invariantsFunctor R G).map f).hom <| fun x hx ↦ by
     simp only [Submodule.submoduleOf, Submodule.mem_comap, Submodule.subtype_apply,
-      LinearMap.mem_range, Rep.invariantsFunctor_map_hom, LinearMap.codRestrict_apply,
-      LinearMap.coe_comp, Submodule.coe_subtype, Function.comp_apply] at hx ⊢
+      LinearMap.mem_range] at hx ⊢
     obtain ⟨a, ha⟩ := hx
-    use f.hom.hom a
-    simpa [← ha] using LinearMap.ext_iff.1 congr(ModuleCat.Hom.hom
-      $(congr(Action.Hom.hom $(A.norm_comm f)))) a
+    use f.hom a
+    simp [Rep.invariantsFunctor, Rep.norm_comm_apply, ha]
 
 -- what is this error about termination??
 def CupProduct (A B : Rep R G) (p q r : ℤ) (h : r = p + q) :
